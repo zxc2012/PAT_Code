bed098a250816767a93253be35d9af3ccd41cb4c
zxc2012 <786698925@qq.com>
2019-11-09 13:57:07 +0800

    jupyter test

--- py/np_test.py
+++ py/np_test.py
@@ -5,6 +5,7 @@ b = np.array([[[1,2,3,4],[1,3,4,5]],[[2,4,7,5],[8,4,3,5]],[[2,5,7,3],[1,5,3,7]]]
 c=np.sort(b,axis=1);
 rg=np.eye(3,6,0,dtype=np.int)
 a=np.arange(16).reshape(4,4)
+s=np.linalg.norm(b,axis=1);
 print(a*np.pi)
 print(random.random())
-print(rg)
+print(s)
--- py/plt_test.py
+++ py/plt_test.py
@@ -1,16 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+
 a=np.arange(0,200,0.001)
 b=np.sin(a);
 # ax=plt.subplots(2,2)
-plt.plot(a[a<np.pi],b[a<np.pi],'g')
+plt.figure()
+plt.plot(a,b,'g')
 plt.plot(a[a>np.pi],b[a>np.pi],'r')
 plt.xlim(0,2*np.pi)
 plt.xlabel("x")
 plt.ylabel("y")
-# plt.show()
+plt.show()
 x=np.array([[0,4],
            [2,3] ])
 print(np.linalg.norm(x,axis=0))
 print(math.sqrt(pow(3,2)+pow(4,2)+pow(4,2)+pow(6,2)+pow(1,2)))
+
+
+
