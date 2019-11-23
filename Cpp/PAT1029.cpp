bc1ea2c91023c83c307a3c784c450c65e7285f5c
zxc2012 <786698925@qq.com>
2019-11-23 15:18:42 +0800

    昨日总结

--- Cpp/PAT1029.cpp
+++ Cpp/PAT1029.cpp
@@ -4,17 +4,24 @@ vector<int> v[2],temp;
 int main(){
     int n[2],i,j;
     for(i=0;i<2;++i){
-        cin>>n[i];v[i].resize(n[i]);
+        scanf("%d",&n[i]);v[i].resize(n[i]);
         for(j=0;j<n[i];++j)
-        cin>>v[i][j];
-        sort(v[i].begin(),v[i].end());
+        scanf("%d",&v[i][j]);
+        //sort(v[i].begin(),v[i].end());
     }
     i=0,j=0;
-    while(i<n[0]&&j<=n[1]||i<=n[0]&&j<n[1]){
-        if(v[0][i]<=v[1][j]&&i<n[0]||j==n[1]){temp.push_back(v[0][i]);++i;}
-        else if(v[0][i]>v[1][j]&&j<n[1]||i==n[0]){temp.push_back(v[1][j]);++j;}
+    while(i<n[0]&&j<n[1]){
+        if(v[0][i]<=v[1][j]){temp.push_back(v[0][i]);++i;}
+        else {temp.push_back(v[1][j]);++j;}
+        if(temp.size()==(n[0]+n[1]+1)/2)printf("%d",temp.back());
     }
-    if(temp.size()%2==0)printf("%d",temp[temp.size()/2-1]);
-    else printf("%d",temp[(temp.size()+1)/2-1]);
+    while(i<n[0]){
+        temp.push_back(v[0][i]);++i;
+        if(temp.size()==(n[0]+n[1]+1)/2)printf("%d",temp.back());
+    }
+    while(j<n[1]){
+        temp.push_back(v[1][j]);++j;
+        if(temp.size()==(n[0]+n[1]+1)/2)printf("%d",temp.back());
+    }
     return 0;
 }
--- Cpp/PAT1089.cpp
+++ Cpp/PAT1089.cpp
@@ -7,7 +7,7 @@ int main(){
     for(i=0;i<2;++i){
         v[i].resize(n);
         for(j=0;j<n;++j)
-        cin>>v[i][j];
+        scanf("%d",&v[i][j]);
     }
     i=0;
     while(i+1<n&&v[1][i]<=v[1][i+1])++i;
@@ -25,16 +25,13 @@ int main(){
         }
     }
     else{
-        printf("Merge Sort\n");i=n;flag=1;
-        while(i>2){
-            i=0;
-            for(j=0;j+flag<n;j+=flag){sort(v[0].begin()+j,v[0].begin()+j+flag);++i;}
-            if(j<n)++i;
+        printf("Merge Sort\n");flag=1;
+        while(flag<=n){
+            for(j=0;j<n;j+=flag)sort(v[0].begin()+j,v[0].begin()+min(j+flag,n));
             if(x==1)break;
             if(v[0]==v[1])x=1;
             flag*=2;
         }
-        if(x==0)sort(v[0].begin(),v[0].end());
         for(j=0;j<n;++j){
             printf("%d",v[0][j]);
             if(j!=n-1)printf(" ");
