d68ddddc6637f5c65564655486f0c61854a3ff8c
zxc2012 <786698925@qq.com>
2019-11-30 17:02:26 +0800

    重做

--- Cpp/PAT1060.cpp
+++ Cpp/PAT1060.cpp
@@ -1,77 +1,65 @@
 #include <bits/stdc++.h>
 using namespace std;
+char a[101],b[101];
+vector<char> v1,v2;
 int main() {
-	double a, b;
-	unsigned char n,st1,st2;
-	int p1, p2;
-	scanf("%d %lf %lf",&n,&a,&b);
-	string s1=to_string(a);
-	if(s1.c_str()[0]!='0') {
-		if (s1.find('.') == string::npos)
-			p1 = s1.size();
-		else {
-			p1 = s1.find('.');
-			s1.erase(p1,1);
-		}
-
-		if (n < s1.size())
-			s1 = s1.substr(0, n);
-		else {
-			s1.insert(s1.end(), n - s1.size(), '0');
-			s1 = s1.substr(0, n);
-		}
-
-	} else {
-		if(s1.find_first_not_of("0.")==string::npos)
-			st1=2;
-		else
-			st1 = s1.find_first_not_of("0.");
-		p1 = 2-st1;
-		if (n < s1.size()-st1)
-			s1 = s1.substr(st1, n);
-		else {
-			s1.insert(s1.end(), n - s1.size()+st1, '0');
-			s1 = s1.substr(st1, n);
-		}
+	int n,i,p1=-1,n1=-1,p2=-1,n2=-1,l1=-1,l2=-1,f1=0,f2=0,ff1=0,ff2=0;
+	scanf("%d %s %s",&n,a,b);
+	for(i=0;i<100;++i){
+		if(a[i]=='.')p1=i;
+		if(b[i]=='.')p2=i;
+		if(a[i]<='9'&&a[i]>'0'&&f1==0){f1=1;n1=i;}
+		if(b[i]<='9'&&b[i]>'0'&&f2==0){f2=1;n2=i;}
+		if(a[i]<46&&ff1==0)l1=i,ff1=1;
+		if(b[i]<46&&ff2==0)l2=i,ff2=1;
+		if(a[i]<46&&b[i]<46)break;		
 	}
-	string s2 = to_string(b);
-	if (s2.c_str()[0] != '0') {
-		if (s2.find('.') == string::npos)
-			p2 = s2.size();
-		else {
-			p2 = s2.find('.');
-			s2.erase(p2,1);
+	int temp[n];fill(temp,temp+n,'0');
+	if(n1==-1){v1.insert(v1.begin(),temp,temp+n);p1=n1;}
+	else{
+		if(l1-n1-(p1>n1?1:0)<n){//zero
+			if(p1>n1){
+				v1.insert(v1.begin(),a+n1,a+p1);
+				v1.insert(v1.end(),a+p1+1,a+l1);
+			}else v1.insert(v1.begin(),a+n1,a+l1);		
 		}
-		if (n < s2.size())
-			s2 = s2.substr(0, n);
-		else {
-			s2.insert(s2.end(), n - s2.size()+st2, '0');
-			s2 = s2.substr(0, n);
+		else{
+ 			if(p1>n1&&p1<n1+n){//no zero
+				v1.insert(v1.begin(),a+n1,a+p1);
+				v1.insert(v1.end(),a+p1+1,a+n1+n+1);		
+			}
+			else v1.insert(v1.begin(),a+n1,a+n1+n);
 		}
-	} else {
-		if(s2.find_first_not_of("0.")==string::npos)
-			st2=2;
-		else
-			st2 = s2.find_first_not_of("0.");
-		p2 = 2-st2 ;
-		if (n < s2.size() - st2)
-			s2 = s2.substr(st2, n);
-		else {
-			s2.insert(s2.end(), n - s2.size() + st2, '0');
-			s2 = s2.substr(st2, n);
+		if(p1==-1)p1=l1;
+	}
+	if(n2==-1){v2.insert(v2.begin(),temp,temp+n);p2=n2;}
+	else{
+		if(l2-n2-(p2>n2?1:0)<n){
+			if(p2>n2){
+				v2.insert(v2.begin(),b+n2,b+p2);
+				v2.insert(v2.end(),b+p2+1,b+l2);
+			}else v2.insert(v2.begin(),b+n2,b+l2);
 		}
+		else{
+			if(p2>n2&&p2<n2+n){
+				v2.insert(v2.begin(),b+n2,b+p2);
+				v2.insert(v2.end(),b+p2+1,b+n2+n+1);
+			}
+			else v2.insert(v2.begin(),b+n2,b+n2+n);
+		} 
+		if(p2==-1)p2=l2;
 	}
-	if (s1 != s2) {
-		printf("NO ");
-		printf("0.%s*10^%d ", s1.c_str(), p1);
-		printf("0.%s*10^%d", s2.c_str(), p2);
+	if(v1==v2&&p1-n1==p2-n2){
+		printf("YES 0.");
+		for(i=0;i<v1.size();++i)printf("%c",v1[i]);
+		printf("*10^%d",p1-n1+(p1<n1&&p1>-1?1:0));
 	}
-
-	else {
-		printf("YES ");
-		printf("0.%s*10^%d", s1.c_str(), p1);
-	}
-
-
+	else{
+		printf("NO 0.");
+		for(i=0;i<v1.size();++i)printf("%c",v1[i]);
+		printf("*10^%d 0.",p1-n1+(p1<n1&&p1>-1?1:0));
+		for(i=0;i<v2.size();++i)printf("%c",v2[i]);
+		printf("*10^%d",p2-n2+(p2<n2&&p2>-1?1:0));
+	} 
 	return 0;
 }
