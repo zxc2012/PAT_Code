76e466c6bb18d942d0d2fd8b18dee74e2b180893
zxc2012 <786698925@qq.com>
2019-12-03 21:52:47 +0800

    注意没说可能存在异常数据(只在某些地方指明1-N)

--- Cpp/PAT1150.cpp
+++ Cpp/PAT1150.cpp
@@ -1,14 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int inf=1e9;
+const int inf=0;
 vector<vector<int>>g;
 struct Node{int sq,sum;};
 vector<Node>v;
 bool cmp(Node a,Node b){return a.sum<b.sum;}
 int main(){
-    int n,m,i,j,c1,c2,x,y,sum,flag=0,start;
+    int n,m,i,j,c1,c2,x,y,sum,start,tail,fa=0;
     scanf("%d%d",&n,&m);
-    g.resize(n+1);vector<int> temp(n+1);
+    g.resize(n+1);
     for(i=0;i<n+1;++i)g[i].resize(n+1,inf);
     for(i=0;i<m;++i){
         scanf("%d%d%d",&c1,&c2,&x);
@@ -18,25 +18,24 @@ int main(){
     scanf("%d",&x);
     for(i=1;i<=x;++i){
         scanf("%d",&c1);
-        y=-1,sum=0,flag=0;
-        fill(temp.begin(),temp.end(),0);
+        y=-1,sum=0,fa=0;
+        unordered_map<int,int>mp;vector<int> temp(n+1);
         for(j=0;j<c1;++j){
-            scanf("%d",&c2);
-            temp[c2]++;
+            scanf("%d",&c2);if(j==c1-1)tail=c2;
+            mp[c2]++;temp[c2]++;
             if(y==-1){y=c2;start=c2;}
             else{
                 sum+=g[y][c2];
-                if(g[y][c2]==inf)break;
+                if(g[y][c2]==inf)fa=1;
                 y=c2;
             }        
         }
         y=0;
-        if(sum>=inf){printf("Path %d: NA (Not a TS cycle)\n",i);continue;}
-        for(j=1;j<=n;++j){
-            if(temp[j]==0||temp[start]<2){printf("Path %d: %d (Not a TS cycle)\n",i,sum);flag=1;break;}
-            else if(temp[j]>1)y++;
+        if(fa==1){printf("Path %d: NA (Not a TS cycle)\n",i);continue;}
+        if(mp.size()<n||tail!=start){printf("Path %d: %d (Not a TS cycle)\n",i,sum);continue;}
+        for(auto it:mp){
+            if(it.second>1)y++;
         }
-        if(flag==1)continue;
         v.push_back({i,sum});
         if(y>1)printf("Path %d: %d (TS cycle)\n",i,sum);
         else printf("Path %d: %d (TS simple cycle)\n",i,sum);
