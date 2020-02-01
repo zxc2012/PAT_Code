0a4815843321d811f13a7edcde1084d2b8d0fb8c
zxc2012 <786698925@qq.com>
2020-02-01 16:43:32 +0800

    dp

--- Cpp/leetcode1155.cpp
+++ Cpp/leetcode1155.cpp
@@ -3,17 +3,17 @@ using namespace std;
 class Solution {
 public:
     int numRollsToTarget(int d, int f, int target) {
-        int t;fill(dp[1]+1,dp[1]+f+1,1);
+        memset(dp,0,sizeof(dp));fill(dp[1]+1,dp[1]+f+1,1);
         for(int i=2;i<=d;++i)for(int j=target;j>=0;--j)
-            for(t=1;t<=min(j,f);++t)
+            for(int t=1;t<=min(j,f);++t)
             dp[i][j]=(dp[i][j]+dp[i-1][j-t])%1000000007;
         return dp[d][target];
     }
 private:
-     long long dp[31][1001];
+    int dp[31][1001];
 };
 int main(){
     Solution s;
-    printf("%lld",s.numRollsToTarget(30,30,500));
+    printf("%d",s.numRollsToTarget(30,30,500));
     return 0;
 }
