84f074f8c1ac4e552519e6b9d50ea73da9553d26
zxc2012 <786698925@qq.com>
2020-02-06 22:05:45 +0800

    tf

--- py/tf_initial.py
+++ py/tf_initial.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
 import pandas as pd
+from tensorflow.keras import optimizers
 print(tf.version)
 tf.estimator.LinearRegressor(feature_columns=s,optimizer=)
 tf.clip_by_global_norm
-dict().i
+my_optimizer = optimizers.SGD()
 pd.read_csv
