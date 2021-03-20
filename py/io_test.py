600b2da629e4c0d8ebaa360995df7e6c7b46a20a
zxc2012 <786698925@qq.com>
2021-03-20 16:45:48 +0800

    cs186

--- py/io_test.py
+++ py/io_test.py
@@ -6,3 +6,14 @@ b=[4,5,6]
 c=tuple(b)
 a.append(c)
 print(a)
+
+class Nstr(int):
+    def __init__(self):
+        if isinstance(arg,str):
+            self.total=0
+            for each in arg:
+                self.total+=ord(each)
+        else :
+            print("error")
+
+    
