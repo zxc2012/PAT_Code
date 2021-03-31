bc6036076b2b12086f6859411d5dbc35834d02fb
zxc2012 <786698925@qq.com>
2021-03-31 10:33:02 +0800

    bug

--- py/io_test.py
+++ py/io_test.py
@@ -5,15 +5,5 @@ a=[1,2,3]
 b=[4,5,6]
 c=tuple(b)
 a.append(c)
+print("Start Test!!")
 print(a)
-
-class Nstr(int):
-    def __init__(self):
-        if isinstance(arg,str):
-            self.total=0
-            for each in arg:
-                self.total+=ord(each)
-        else :
-            print("error")
-
-    
