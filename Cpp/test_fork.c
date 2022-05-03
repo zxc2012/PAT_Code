b53e0924d2c37155f436f7fe22ef71cbf8a077e5
zxc2012 <786698925@qq.com>
2022-05-03 16:02:19 +0800

    exec

--- Cpp/test_fork.c
+++ Cpp/test_fork.c
@@ -1,17 +1,24 @@
-#include<sys/types.h>
-#include<stdlib.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <signal.h>
+#include <string.h>
 
-void signal_callback_handler(int signum) {
-  printf("Caught signal!\n");
-  exit(1);
-}
-int main() {
-  struct sigaction sa;
-  sa.sa_flags = 0;
-  sigemptyset(&sa.sa_mask);
-  sa.sa_handler = signal_callback_handler;
-  sigaction(SIGINT, &sa, NULL);
-  while (1) {}
+int main(int argc, char* argv[]){
+    pid_t cpid,tcpid;
+    cpid = fork();
+    int status;
+    pid_t mypid=getpid();
+    if (cpid > 0) {               /* Parent Process */
+        tcpid = wait(&status);
+        printf("%d wait%d(%d)",mypid,tcpid,status);
+    } else if (cpid == 0) {      /* Child Process */
+        mypid = getpid();
+        char *myargs[3];
+        myargs[0] = strdup("wc");   // program: "wc" (word count)
+        myargs[1] = strdup("test.cpp"); // argument: file to count
+        myargs[2] = NULL;           // marks end of array
+        execvp(myargs[0], myargs);  // runs word count
+        printf("this shouldn't print out.\n");
+    }
 }
