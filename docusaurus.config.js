b5de8ae484ad18fa23ebbb5363adc2cc20d45b15
zxc2012 <786698925@qq.com>
2022-10-20 14:02:27 +0800

    bug

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -31,13 +31,6 @@ const config = {
           editUrl:
             'https://github.com/zxc2012/zxc2012.github.io/tree/master',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/zxc2012/zxc2012.github.io/tree/master',
-        },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
