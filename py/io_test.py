654ff3440b7e7097d45aa7491bea4d1c74aa2241
zxc2012 <786698925@qq.com>
2023-02-25 22:58:14 +0800

    paging

--- py/io_test.py
+++ py/io_test.py
@@ -5,10 +5,17 @@ xpoints = []
 for i in range(2004,2024):
     xpoints.append(i)
 ypoints = [85000,85000,85000,85000,85000,163000,85000,85000,85000,85000,124000,172500,233000,236000,199000,190098,201011,275000,308613,483927]
-for a,b in zip(xpoints[16:],ypoints[16:]):
-    plt.text(a, b, '%d' % b, fontsize=9)
-plt.plot(xpoints,ypoints,marker = 'o')
-plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))
 
-plt.gca().yaxis.set_major_locator(MaxNLocator(integer=True))
-plt.savefig('../x.jpg')
+fig, ax1 = plt.subplots()
+
+ax1.plot(xpoints,ypoints,marker = 'o')
+ax1.legend(['Q'],ncol=1,loc=2)
+
+
+ax2 = ax1.twinx()
+y = [850,850,850,163,850,850,850,85,124,172,233,236,199,190,201,275,308,483]
+ax2.plot(xpoints,y,color="red",marker="*")
+ax2.legend(['V'],ncol=1)
+
+plt.show()
+
--- py/plt_test.py
+++ py/plt_test.py
@@ -1,14 +1,40 @@
-a= input()
-a=a[1:-1]
-ds=[]
-ns=[]
-b=[]
-for i in a.split(','):
-    b.append(eval(i))
-ds.append(b[0])
-ns.append(0)
-n=len(b)
-for i in range(n):
-    ds.append(ns[i-1]+b[i])
-    ns.append(max(ns[i-1],ds[i-1]))
-print(max(ds[n-1],ns[n-1]))
+import pandas as pd 
+import matplotlib.pyplot as plt
+import numpy as np
+df = pd.read_csv("weather.csv",encoding='gb2312')
+# df.plot()
+x = df['时间']
+y1 = df['日平均气温']
+y2 = df['日最高气温']
+y3 = df['日最低气温']
+z = df['生长速率']
+
+fig = plt.figure(figsize=(15,9))
+# left
+ax1 = fig.add_subplot(111)
+
+ax1.plot(list(x),list(y2),color='orange')
+ax1.plot(list(x),list(y1),color="green")
+ax1.plot(list(x),list(y3),color="skyblue")
+ax1.legend(['日最高气温','日平均气温','日最低气温'],ncol=1,loc=2)
+# 设置y轴标题
+ax1.set_ylabel('温度:摄氏度')
+# 设置x轴刻度
+ax1.set_xticks([0,5,10,15,20,25])
+# 设置x轴标题
+ax1.set_xlabel('日期')
+
+#right
+realx = []
+realz = []
+ax2 = ax1.twinx()
+for point in range(len(list(z))):
+    if np.isnan(z[point]) == False:
+        realx.append(x[point])
+        realz.append(z[point])
+ax2.plot(realx,realz,color="red",marker="*")
+ax2.set_ylabel('生长速率: cm/d')
+ax2.legend(['生长速率'],ncol=1)
+# 设置标注
+
+plt.show()
